ea65712f3814537aad9a05e874f89570f860c3a7
Jesse Bernier <jesse_bernier@hotmail.com>
2017-11-10 18:37:44 -0500

    added animals to word list

--- wordList.js
+++ wordList.js
@@ -4,7 +4,7 @@ var Word = require('./word.js');
 //function to pick nd return random word from list
 module.exports.newWord = function (){
 	//word list
-	var wordOptions = ["dog","cat","elephant","rhinoceros"];
+	var wordOptions = ["dog","cat","elephant","rhinoceros","ferret","hamster","pig","aardvark","armadillo","jellyfish","tardigrade","ameoba","pangolin","octopus"];
 	// pick rabdom one
 	var randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 	// create as object
